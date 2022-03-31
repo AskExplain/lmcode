d13bba6d04f0e40342c94bfdb89d9ad0b24761a7
DavidB-XI <80179887+DavidB-XI@users.noreply.github.com>
2022-03-31 17:00:01 +1000

    Update lmcode (previous estimation errors)

--- R/initialise.lmcode.R
+++ R/initialise.lmcode.R
@@ -6,9 +6,17 @@ initialise.lmcode <- function(data_list,
 
   main.code <- list(lm.code = array(rnorm(dim(data_list$x)[1]*config$j_dim),dim=c(dim(data_list$x)[1],config$j_dim)))
 
-  main.parameters <- list(u = array(rnorm(dim(data_list$z)[2]*config$j_dim),dim=c(dim(data_list$z)[2],config$j_dim)),
-                          beta = array(rnorm(dim(data_list$x)[2]*config$j_dim),dim=c(dim(data_list$x)[2],config$j_dim))
-                          )
+  # main.parameters <- list(alpha = irlba::irlba(data_list$y,nv = config$j_dim,maxit = 5)$v,
+  #                         beta = irlba::irlba(data_list$x,nv = config$j_dim,maxit = 5)$v,
+  #                         u = irlba::irlba(data_list$z,nv = config$j_dim,maxit = 5)$v
+  #                         )
+
+  main.parameters <- list(alpha = array(rnorm(dim(data_list$y)[2]*config$j_dim),dim=c(dim(data_list$y)[2],config$j_dim)),
+                          beta = array(rnorm(dim(data_list$x)[2]*config$j_dim),dim=c(dim(data_list$x)[2],config$j_dim)),
+                          u = array(rnorm(dim(data_list$z)[2]*config$j_dim),dim=c(dim(data_list$z)[2],config$j_dim))
+  )
+
+
 
 
   return(list(main.parameters=main.parameters,
