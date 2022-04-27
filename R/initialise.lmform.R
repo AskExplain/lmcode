a564152c7b4e66e95fae273157db83b9eef16e1a
DavidB-XI <80179887+DavidB-XI@users.noreply.github.com>
2022-04-28 09:49:37 +1000

    Testing random effects

--- R/initialise.lmform.R
+++ R/initialise.lmform.R
@@ -37,6 +37,7 @@ initialise.lmform <- function(data_list,
 
 
 
+
   main.parameters$main.fixed$alpha <- t((MASS::ginv(t(main.form$fixed_form)%*%(main.form$fixed_form))%*%t(main.form$fixed_form)%*%(data_list$y)))
   main.parameters$main.fixed$beta <- t((MASS::ginv(t(main.form$fixed_form)%*%(main.form$fixed_form))%*%t(main.form$fixed_form)%*%(data_list$x)))
   main.parameters$main.random$u <- t((MASS::ginv(t(main.form$fixed_form)%*%(main.form$fixed_form))%*%t(main.form$fixed_form)%*%(data_list$z)))
@@ -47,13 +48,13 @@ initialise.lmform <- function(data_list,
 
   main.parameters$main.random$var_Y <- diag(diag((main.parameters$main.random$residuals)%*%t(main.parameters$main.random$residuals)/dim(data_list$y)[1]))
   main.parameters$main.random$D <- diag(diag(main.parameters$main.random$var_Y - data_list$z%*%t(data_list$z)))
-  diag(main.parameters$main.random$D)[diag(main.parameters$main.random$D)<0] <- 1e-5
+  diag(main.parameters$main.random$D)[diag(main.parameters$main.random$D)<0] <- 1e-8
   main.parameters$main.random$V_inv <- diag(1/diag(main.parameters$main.random$D)) - diag(1/diag(main.parameters$main.random$D))%*%main.form$random_form%*%MASS::ginv(MASS::ginv(t(main.parameters$main.random$u)%*%(main.parameters$main.random$u)) + t(main.form$random_form)%*%diag(1/diag(main.parameters$main.random$D))%*%main.form$random_form)%*%t(main.form$random_form)%*%diag(1/diag(main.parameters$main.random$D))
   main.parameters$main.random$P <- main.parameters$main.random$V_inv - main.parameters$main.random$V_inv%*%main.form$fixed_form%*%MASS::ginv(t(main.form$fixed_form)%*%main.parameters$main.random$V_inv%*%main.form$fixed_form)%*%t(main.form$fixed_form)%*%main.parameters$main.random$V_inv
   main.parameters$main.random$sigma_w <- (1/dim(data_list$z)[1])*c((t(main.parameters$main.random$residuals - data_list$z%*%main.parameters$main.random$u%*%MASS::ginv(t(main.parameters$main.random$u)%*%(main.parameters$main.random$u))%*%t(main.parameters$main.fixed$alpha))%*%(main.parameters$main.random$residuals - data_list$z%*%main.parameters$main.random$u%*%MASS::ginv(t(main.parameters$main.random$u)%*%(main.parameters$main.random$u))%*%t(main.parameters$main.fixed$alpha)) - sum(diag(main.parameters$main.random$P))))
   main.parameters$main.random$sigma_H <- c(t(data_list$y)%*%main.parameters$main.random$P%*%data_list$y)/(dim(data_list$y)[1])
   main.parameters$main.random$G <- diag(diag((((1/main.parameters$main.random$sigma_w)*main.parameters$main.random$u%*%t(main.parameters$main.random$u) - t(data_list$z)%*%main.parameters$main.random$P%*%data_list$z))))
-  diag(main.parameters$main.random$G)[diag(main.parameters$main.random$G)<0] <- 1e-5
+  diag(main.parameters$main.random$G)[diag(main.parameters$main.random$G)<0] <- 1e-8
   main.parameters$main.random$u <- main.parameters$main.random$G%*%t(data_list$z)%*%main.parameters$main.random$V_inv%*%(main.parameters$main.random$residuals)%*%main.parameters$main.fixed$alpha
 
 
--- R/lmform.R
+++ R/lmform.R
@@ -104,25 +104,22 @@ update_set <- function(data_list,
 
   main.parameters$main.fixed$alpha <- t((MASS::ginv(t(main.parameters$main.random$V_inv%*%main.form$fixed_form)%*%(main.parameters$main.random$V_inv%*%main.form$fixed_form))%*%t(main.parameters$main.random$V_inv%*%main.form$fixed_form)%*%main.parameters$main.random$V_inv%*%(data_list$y)))
   main.parameters$main.fixed$beta <- t((MASS::ginv(t(main.parameters$main.random$V_inv%*%main.form$fixed_form)%*%(main.parameters$main.random$V_inv%*%main.form$fixed_form))%*%t(main.parameters$main.random$V_inv%*%main.form$fixed_form)%*%main.parameters$main.random$V_inv%*%(data_list$x)))
-  main.parameters$main.fixed$alpha_projection <- t(main.parameters$main.fixed$alpha)%*%MASS::ginv(main.parameters$main.fixed$alpha%*%t(main.parameters$main.fixed$alpha))
-  main.parameters$main.random$residuals <- data_list$y-data_list$x%*%main.parameters$main.fixed$beta%*%main.parameters$main.fixed$alpha_projection
+  main.parameters$main.random$residuals <- data_list$y%*%main.parameters$main.fixed$alpha-data_list$x%*%main.parameters$main.fixed$beta
 
   main.form$random_form <- (data_list$z%*%main.parameters$main.random$u)%*%MASS::ginv(t(main.parameters$main.random$u)%*%main.parameters$main.random$u)
   main.form$fixed_form <- (data_list$y%*%main.parameters$main.fixed$alpha)%*%MASS::ginv(t(main.parameters$main.fixed$alpha)%*%main.parameters$main.fixed$alpha)
 
   main.parameters$main.random$var_Y <- diag(diag((main.parameters$main.random$residuals)%*%t(main.parameters$main.random$residuals))/dim(data_list$y)[1])
 
-  main.parameters$main.random$D <- diag(diag(main.parameters$main.random$var_Y - data_list$z%*%main.parameters$main.random$G%*%t(data_list$z)))
-  diag(main.parameters$main.random$D)[diag(main.parameters$main.random$D)<0] <- 1e-5
+  main.parameters$main.random$D <- (diag(diag(main.parameters$main.random$var_Y - data_list$z%*%main.parameters$main.random$G%*%t(data_list$z))))
+  diag(main.parameters$main.random$D)[diag(main.parameters$main.random$D)<0] <- 1e-8
 
-  main.parameters$main.random$V_inv <- diag(1/diag(main.parameters$main.random$D)) - diag(1/diag(main.parameters$main.random$D))%*%main.form$random_form%*%MASS::ginv(MASS::ginv(t(main.parameters$main.random$u)%*%main.parameters$main.random$G%*%(main.parameters$main.random$u)) + t(main.form$random_form)%*%diag(1/diag(main.parameters$main.random$D))%*%main.form$random_form)%*%t(main.form$random_form)%*%diag(1/diag(main.parameters$main.random$D))
+  main.parameters$main.random$V_inv <- diag(dim(data_list$y)[1]) - data_list$z%*%(main.parameters$main.random$G - main.parameters$main.random$G%*%main.parameters$main.random$u%*%MASS::ginv(MASS::ginv(t(main.form$random_form)%*%(main.form$random_form))+(t(main.parameters$main.random$u)%*%main.parameters$main.random$G%*%main.parameters$main.random$u))%*%t(main.parameters$main.random$u)%*%main.parameters$main.random$G)%*%t(data_list$z)
   main.parameters$main.random$P <- main.parameters$main.random$V_inv - main.parameters$main.random$V_inv%*%data_list$x%*%MASS::ginv(t(data_list$x)%*%main.parameters$main.random$V_inv%*%data_list$x)%*%t(data_list$x)%*%main.parameters$main.random$V_inv
 
-  main.parameters$main.random$sigma_w <- main.parameters$main.random$sigma_w+(1/dim(data_list$z)[1])*c((t(main.parameters$main.random$residuals - data_list$z%*%main.parameters$main.random$u%*%main.parameters$main.fixed$alpha_projection)%*%(main.parameters$main.random$residuals - data_list$z%*%main.parameters$main.random$u%*%MASS::ginv(t(main.parameters$main.random$u)%*%(main.parameters$main.random$u))%*%t(main.parameters$main.fixed$alpha)) - main.parameters$main.random$sigma_w*sum(diag(main.parameters$main.random$P))))
-  main.parameters$main.random$sigma_H <- c(t(data_list$y)%*%main.parameters$main.random$P%*%data_list$y)/(dim(data_list$y)[1])
-
-  main.parameters$main.random$G <- main.parameters$main.random$G+(1/dim(data_list$z)[2])*diag(diag((((1/main.parameters$main.random$sigma_w)*(main.parameters$main.random$u)%*%t(main.parameters$main.random$u) - main.parameters$main.random$G%*%t(data_list$z)%*%main.parameters$main.random$P%*%data_list$z%*%main.parameters$main.random$G))))
-  diag(main.parameters$main.random$G)[diag(main.parameters$main.random$G)<0] <- 1e-5
+  main.parameters$main.random$sigma_w <- main.parameters$main.random$sigma_w+(1/dim(data_list$z)[1])*c((t(data_list$y%*%main.parameters$main.fixed$alpha - data_list$x%*%main.parameters$main.fixed$beta - data_list$z%*%main.parameters$main.random$u)%*%(data_list$y%*%main.parameters$main.fixed$alpha - data_list$x%*%main.parameters$main.fixed$beta - data_list$z%*%main.parameters$main.random$u) - main.parameters$main.random$sigma_w*sum(diag(main.parameters$main.random$P))))
+  main.parameters$main.random$G <- main.parameters$main.random$G+diag(diag((((1/main.parameters$main.random$sigma_w)*(main.parameters$main.random$u)%*%t(main.parameters$main.random$u) - main.parameters$main.random$G%*%t(data_list$z)%*%main.parameters$main.random$P%*%data_list$z%*%main.parameters$main.random$G))))
+  diag(main.parameters$main.random$G)[diag(main.parameters$main.random$G)<0] <- 1e-8
 
   main.parameters$main.random$u <- main.parameters$main.random$G%*%t(data_list$z)%*%main.parameters$main.random$V_inv%*%(main.parameters$main.random$residuals)%*%main.parameters$main.fixed$alpha
 
